77124aa3aeeb22c7867139dfba9c73eae74d703e
lisak <jan.liska@mopetcz.cz>
2013-08-31 18:31:16 +0200

    All basic functionality tested

--- src/main/webapp/WEB-INF/resources/js/map.js
+++ src/main/webapp/WEB-INF/resources/js/map.js
@@ -6,7 +6,7 @@ var mapOptions;
 //hashMap with prepared markers
 var markerMap = {};
 //jQuery object with custom controls
-var customControls;
+//var customControls;
 
 //set map to facelifted mode
 google.maps.visualRefresh = true;
@@ -14,178 +14,176 @@ google.maps.visualRefresh = true;
 //put marker into "selected" state 
 //& deselect previous selected marker if there is any 
 function toggleIcon(marker) {
-	if(selectedMarker != null) {
-		selectedMarker.setIcon(selectedMarker.icon.replace("-selected", ""));
-	}
-	
-	if(marker != null) {
-		marker.setIcon(marker.icon.replace(".", "-selected."));
-		selectedMarker = marker;
-	}
+    if (selectedMarker != null) {
+        selectedMarker.setIcon(selectedMarker.icon.replace("-selected", ""));
+    }
+
+    if (marker != null) {
+        marker.setIcon(marker.icon.replace(".", "-selected."));
+        selectedMarker = marker;
+    }
 }
 
 //closure callback function for click event
 function onClickMarker(marker) {
-	return function() {
-		console.log("Marker: " + marker.profile + " clicked.");
-		toggleIcon(marker);
-		$('#profileBody').load('/profile/' + marker.profile, function() {
-			//when the new profile loads...
-			
-			//register mapLink callback for new profile
-			$('a.mapLink').click(onClickMapLink);
-			//align profile container on with the map
-			$('#profile').css('top', 0);
-			//and show the profile
-			$('#profile').show();
-		});
-	};
+    return function () {
+        console.log("Marker: " + marker.profile + " clicked.");
+        toggleIcon(marker);
+        $('#profileBody').load('/profile/' + marker.profile, function () {
+            //when the new profile loads...
+
+            //register mapLink callback for new profile
+            $('a.mapLink').click(onClickMapLink);
+            //align profile container on with the map & show the profile
+            $('#profile').css('top', 0).show();
+        });
+    };
 }
 
 function onClickMapLink(event) {
-	event.preventDefault();
-	
-	//align profile container on with the map
-	$('#profile').css('top', 0);
-	//...and scroll to the top
-	$(document).scrollTop(0);
-	showMap();
+    event.preventDefault();
+
+    //align profile container on with the map
+    $('#profile').css('top', 0);
+    //...and scroll to the top
+    $(document).scrollTop(0);
+    showMap();
     //take only the last part of the URL - it's the marker code
-	highlightMarker($(this).attr('href').replace(/^.*[\/]/, ""));
+    highlightMarker($(this).attr('href').replace(/^.*[\/]/, ""));
 }
 
 //prepare markers in the format suitable for map set-up
 function prepareMarkers(markerData) {
-	var marker;
-	
-	$.each(markerData, function(_k, category) {
-		$.each(category.items, function(_k, mData) {
-			marker = new google.maps.Marker({
-				position:  new google.maps.LatLng(mData.lat, mData.lng),
-				//map is lazy-initialized by showMap function
-				map: null,
-				profile: mData.name,
-				options: {icon: category.icon},
-				category: category.categoryName,
-				title: mData.title
-			});
-			
-			console.log("Marker " + mData.name + " prepared.");
-			
-			//add marker to global Map object
-			markerMap[mData.name] = marker;
-		});
-	});
+    var marker;
+
+    $.each(markerData, function (_k, category) {
+        $.each(category.items, function (_k, mData) {
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(mData.lat, mData.lng),
+                //map is lazy-initialized by showMap function
+                map: null,
+                profile: mData.name,
+                options: {icon: category.icon},
+                category: category.categoryName,
+                title: mData.title
+            });
+
+            console.log("Marker " + mData.name + " prepared.");
+
+            //add marker to global Map object
+            markerMap[mData.name] = marker;
+        });
+    });
 }
 
 function highlightMarker(markerCode) {
-	console.log("MarkerCode: " + markerCode);
-	var _mark = markerMap[markerCode];
-	
-	toggleIcon(_mark);
-	//center map to the marker
-	googleMap.setCenter(_mark.getPosition());
+    console.log("MarkerCode: " + markerCode);
+    var _mark = markerMap[markerCode];
+
+    toggleIcon(_mark);
+    //center map to the marker
+    googleMap.setCenter(_mark.getPosition());
 }
 
 function filterMarkers(categoryName, filterValue) {
-	$.each(markerMap, function(markerName, marker){
-		if(marker.category===categoryName) {
-			marker.setVisible(filterValue);
-		}
-	});
+    $.each(markerMap, function (markerName, marker) {
+        if (marker.category === categoryName) {
+            marker.setVisible(filterValue);
+        }
+    });
 }
 
 //here we set up a map with predefined markers
 function prepareMapOptions(markerData) {
-	  var mapCenter = new google.maps.LatLng(50.073665, 14.433141);
-	  //prepare global variable with map configuration
-	  mapOptions = {
-	    zoom: 13,
-	    minZoom: 10,
-	    center: mapCenter,
-	    mapTypeControlOptions: {
-		       style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-		       position: google.maps.ControlPosition.RIGHT_TOP
-		     },
-		zoomControlOptions: {
-			style: google.maps.ZoomControlStyle.SMALL
-		},
-		navigationControl: false,
-		scrollwheel: true,
-		streetViewControl: true, 
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
-	  };
-	  
-	  //set up custom map buttons
-	  setupCustomControls();
+    var mapCenter = new google.maps.LatLng(50.073665, 14.433141);
+    //prepare global variable with map configuration
+    mapOptions = {
+        zoom: 13,
+        minZoom: 10,
+        center: mapCenter,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            position: google.maps.ControlPosition.RIGHT_TOP
+        },
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL
+        },
+        navigationControl: false,
+        scrollwheel: true,
+        streetViewControl: true,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    //set up custom map buttons
+    setupCustomControls();
 }
 
 function setupCustomControls() {
-	//set global variable
-	customControl = $('#mapCustomControls');
-	
-	//set onclick handler for mapClose
-	$('#closeMap').click(function(event){
-			event.preventDefault();
-			hideMap();
-	});
+    //set global variable
+    customControl = $('#mapCustomControls');
+
+    //set onclick handler for mapClose
+    $('#closeMap').click(function (event) {
+        event.preventDefault();
+        hideMap();
+    });
 }
 
 // create and show map (based on config stored in global vars.)
 function showMap() {
-	console.log("showMap called...");
-	if(googleMap===null) {
-		googleMap =  new google.maps.Map($("#map-canvas")[0], mapOptions);
-		//set up markers
-		$.each(markerMap, function(markerCode, marker) {
-			marker.setMap(googleMap);
-			google.maps.event.addListener(marker, 'click', onClickMarker(marker));
-		});
-		
-		console.log("Attaching custom controls...");
-		//attach custom controls
-		googleMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(customControl[0]);
-		
-		//attach onClick handler to filters
-		$(".markerFilter").click(function(event) {
-			var filterValue = this.checked;
-			var filterId = $(this).attr("id");
-			
-			filterMarkers(filterId, filterValue);
-			console.log("Filter: " + $(this).attr("id") + " clicked, new value: " + this.checked);
-		});
-		
-		
-		//customControl is hidden initialy, show it now
-		customControl.show(0);
-	}
+    console.log("showMap called...");
+    if (googleMap === null) {
+        googleMap = new google.maps.Map($("#map-canvas")[0], mapOptions);
+        //set up markers
+        $.each(markerMap, function (markerCode, marker) {
+            marker.setMap(googleMap);
+            google.maps.event.addListener(marker, 'click', onClickMarker(marker));
+        });
 
-	//hide article
-	$('article.main').hide();
-	
-	var mapCanvas = $('#map-canvas');
+        console.log("Attaching custom controls...");
+        //attach custom controls
+        googleMap.controls[google.maps.ControlPosition.TOP_RIGHT].push(customControl[0]);
 
-	//show prepared map
-	mapCanvas.show();
-	//set map height ()
-	mapCanvas.height($(window).height() - mapCanvas.position().top - 20);
+        //attach onClick handler to filters
+        $(".markerFilter").click(function (event) {
+            var filterValue = this.checked;
+            var filterId = $(this).attr("id");
+
+            filterMarkers(filterId, filterValue);
+            console.log("Filter: " + $(this).attr("id") + " clicked, new value: " + this.checked);
+        });
+
+
+        //customControl is hidden initialy, show it now
+        customControl.show(0);
+    }
+
+    //hide article
+    $('article.main').hide();
+
+    var mapCanvas = $('#map-canvas');
+
+    //show prepared map
+    mapCanvas.show();
+    //set map height ()
+    mapCanvas.height($(window).height() - mapCanvas.position().top - 20);
 }
 
 function hideMap() {
-	$('#map-canvas').hide();
-	$('article.main').show();
+    $('#map-canvas').hide();
+    $('article.main').show();
 }
 
 /******* DocumentReady functions ******/
 
-$(document).ready(function() {
-	//first, get POI JSON loaded & parsed.
-	$.getJSON("/poi.json", function(data) {
-		//prepare map configuration
-		prepareMapOptions();
-		//prepare hashMap of markers
-		prepareMarkers(data);
-	});
+$(document).ready(function () {
+    //first, get POI JSON loaded & parsed.
+    $.getJSON("/poi.json", function (data) {
+        //prepare map configuration
+        prepareMapOptions();
+        //prepare hashMap of markers
+        prepareMarkers(data);
+    });
 });	
 
 
